d4bb4a23ef687f16c7130ecb74269d04ec09f428
Desktop <vitalii-kozak@ukr.net>
2018-01-25 00:33:42 +0200

    24.01.2017

---
+++ src/lesson1/Main.java
@@ -0,0 +1,52 @@
+package lesson1;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class Main {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+
+        Class<?> cls = MyClass.class;
+        
+        
+        Method[] methods = cls.getDeclaredMethods();
+
+        for (Method method : methods) {
+            int modifiers = method.getModifiers();
+            if ((method.isAnnotationPresent(Test.class)) && (Modifier.isStatic(modifiers))) {
+                Test test = method.getAnnotation(Test.class);
+                int aaaResult = (Integer) method.invoke(null, test.a(), test.b());
+                System.out.println(aaaResult);
+            }
+        }
+
+        MyClass myClass = new MyClass();
+        cls = myClass.getClass();
+
+        for (Method method : methods) {
+            int modifiers = method.getModifiers();
+            if ((method.isAnnotationPresent(Test.class)) && (!Modifier.isStatic(modifiers))) {
+                Test test = method.getAnnotation(Test.class);
+                int aaaResult = (Integer) method.invoke(myClass, test.a(), test.b());
+                System.out.println(aaaResult);
+            }
+        }
+
+    }
+
+}
+
+
+class MyClass {
+    @Test(a = 4, b = 5)
+    public static int sum1(int a, int b) {
+        return a + b;
+    }
+
+    @Test( a = 4, b = 3)
+    public  int sum2(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/lesson1/Test.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
+public @interface Test {
+    int a();
+    int b();
+}
